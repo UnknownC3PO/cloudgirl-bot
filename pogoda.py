c81d5582bee1dca1d1d6173e0e5358abe767c759
UnknownC3PO <bitsyuk123@gmail.com>
2021-09-05 02:56:55 +0300

    changed pogoda.py

--- pogoda.py
+++ pogoda.py
@@ -2,10 +2,8 @@ import telebot
 import requests
 import time
 import config
-from telebot import types
 
 bot = telebot.TeleBot(config.TOKEN)
-user_data=[]
 
 
 @bot.message_handler(commands=['start'])
@@ -13,21 +11,6 @@ def start(message):
     sti = open('static/AnimatedSticker.tgs', 'rb')
     bot.send_sticker(message.chat.id, sti)
     bot.send_message(message.chat.id, 'Hello! What city do you live in ?')
-
-@bot.message_handler(commands=['button'])
-def but_ton(message):
-    markup = types.InlineKeyboardMarkup(row_width=1)
-    item = types.InlineKeyboardMarkup('Мой город', callback_data='city_cache')
-    markup.add(item)
-
-    bot.send_message(message.chat.id, 'Are you want add you city in list ?', reply_markup=markup)
-
-@bot.callback_query_handler(func=lambda call:True)
-def callback(call):
-    if call.message:
-        if call.data == 'city_cache':
-            #user_data.append(message.from_user.id)
-            bot.send_message(call.message.chat.id, 'Pls, write your city here, if you want add in hot_list')
 
 
 @bot.message_handler(commands=['help'])
