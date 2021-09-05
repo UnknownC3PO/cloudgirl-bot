0da4eccfc22e7f867584ee28378d36ab737b7f86
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-09-05 22:15:44 +0300

    Update pogoda.py

--- pogoda.py
+++ pogoda.py
@@ -78,7 +78,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url="https://somename.herokuapp.com/bot") #пробывал и https://somename.herokuapp.com
+    bot.set_webhook(url="https://cloudgirl-bot.herokuapp.com/") 
     return "!", 200
 
 server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
