ff46fa2d046a00572fb7ce84c6f8f953695ea156
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-09-05 22:42:56 +0300

    Update pogoda.py

--- pogoda.py
+++ pogoda.py
@@ -67,4 +67,18 @@ def but_ton(message):
                 bot.reply_to(message, 'Incorrect, try again')
     user_data.clear()
 
-bot.polling(none_stop=True)
+if "HEROKU_BOT" in list(os.environ.keys()):
+    logger = telebot.logger
+    telebot.logger.setLevel(logging.INFO)
+
+    server = Flask(__name__)
+    @server.route("/bot", methods=['POST'])
+    def getMessage():
+        bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+        return "!", 200
+    @server.route("/")
+    def webhook():
+        bot.remove_webhook()
+        bot.set_webhook(url="https://min-gallows.herokuapp.com/bot") # этот url нужно заменить на url вашего Хероку приложения
+        return "?", 200
+    server.run(host="0.0.0.0", port=os.environ.get('PORT', 80))
