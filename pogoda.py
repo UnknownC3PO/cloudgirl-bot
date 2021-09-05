95315ed52b6b4a05268eacac84f1e087de41ed02
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-09-05 22:21:33 +0300

    Update pogoda.py

--- pogoda.py
+++ pogoda.py
@@ -70,7 +70,7 @@ def but_ton(message):
     user_data.clear()
 
 
-@server.route('/', methods=['POST'])
+@server.route('/' + config.TOKEN, methods=['POST'])
 def getMessage():
     json_string = request.get_data().decode('utf-8')
     update = telebot.types.Update.de_json(json_string)
@@ -81,11 +81,10 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://cloudgirl-bot.herokuapp.com/')
+    bot.set_webhook(url='https://your_heroku_project.com/' + config.TOKEN)
     return "!", 200
 
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-
 
