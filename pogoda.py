8ee25b5d576fd45d90289af45835e022cd699c44
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-09-05 22:22:06 +0300

    Update pogoda.py

--- pogoda.py
+++ pogoda.py
@@ -81,7 +81,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://your_heroku_project.com/' + config.TOKEN)
+    bot.set_webhook(url='https://cloudgirl-bot.herokuapp.com/' + config.TOKEN)
     return "!", 200
 
 
