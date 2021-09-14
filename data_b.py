b8ceac959cd4cc1c934d4603cf2344a97a0c9161
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-09-15 00:25:21 +0300

    Update data_b.py

--- data_b.py
+++ data_b.py
@@ -1,41 +1,59 @@
-from bot import bot
-import config
-import data_b
-import get_weather
-import buttons
+import os
+import json
+
+if not os.path.isfile('users_db.json'):
+    with open('users_db.json', 'w') as db:
+        json.dump({'users': []}, db)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    sti = open('static/AnimatedSticker.tgs', 'rb')
-    bot.send_sticker(message.chat.id, sti)
-    bot.send_message(message.chat.id, 'Hello! What city do you live in ?')
+def check_key(data_users, user_id):
+    for user in data_users['users']:
+        if user_id == int(user['id']):
+            return False
+    return True
 
 
-@bot.message_handler(commands=['help'])
-def helper(message):
-    bot.send_message(message.chat.id,
-                     '''Commands:\nget - for get info about user.\nadd (your city) - add\
- or create new user.\ndel (your city) - delete user.\nupdate (your city) - update user.''')
+def get_user(user_id):
+    with open('users_db.json', 'r') as db_get:
+        data_users = json.load(db_get)
+    for user in data_users['users']:
+        if user_id == int(user['id']):
+            return str(user['city'])
+    return 'User does not exist.'
 
 
-@bot.message_handler(content_types=['text'])
-def change(message):
-    if message.text.lower() == 'get':
-        bot.send_message(message.chat.id, data_b.get_user(message.chat.id))
-    elif message.text[0:3].lower() == 'add':
-        if data_b.add_user(message.chat.id, message.text[4:]):
-            bot.send_message(message.chat.id, 'Added.', reply_markup=buttons.city_button(message.text[4:]))
-        else:
-            bot.send_message(message.chat.id, 'User already exists')
-    elif message.text[0:6].lower() == 'update':
-        bot.send_message(message.chat.id, data_b.update_user(message.chat.id, message.text[7:]),
-                         reply_markup=buttons.city_button(message.text[7:]))
-    elif message.text[0:3].lower() == 'del':
-        bot.send_message(message.chat.id, data_b.del_user(message.chat.id, message.text[4:]))
-    else:
-        bot.send_message(message.chat.id, get_weather.get_weather_info(message.text))
+def add_user(user_id, city):
+    try:
+        with open('users_db.json', 'r+') as db_add:
+            data_users = json.load(db_add)
+            if check_key(data_users, user_id):
+                data_users['users'].append({'id': user_id, 'city': city})
+                db_add.seek(0)
+                json.dump(data_users, db_add)
+                db_add.truncate()
+            return True
+    except NameError:
+        return False
 
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+def del_user(user_id, city):
+    try:
+        with open('users_db.json', 'r+') as user_remove:
+            data_users = json.load(user_remove)
+            user_remove.seek(0)
+            data_users['users'].remove({'id': user_id, 'city': city})
+            json.dump(data_users, user_remove)
+            user_remove.truncate()
+        return 'Deleted.'
+    except ValueError:
+        return 'Wrong city or your city not in list. Try again!'
+
+
+def update_user(user_id, city):
+    try:
+        user_for_update = get_user(user_id)
+        del_user(user_id, user_for_update)
+        add_user(user_id, city)
+        return 'Updated'
+    except ValueError:
+        return 'User does not exist.'
