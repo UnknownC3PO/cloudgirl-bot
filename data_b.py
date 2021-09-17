434ac73247b09a81addae4aa401fb5ec14671cd3
UnknownC3PO <80990115+UnknownC3PO@users.noreply.github.com>
2021-09-17 10:18:33 +0300

    Update data_b.py

--- data_b.py
+++ data_b.py
@@ -1,6 +1,5 @@
 import os
 import json
-import logging
 
 if not os.path.isfile('users_db.json'):
     with open('users_db.json', 'w') as db:
@@ -11,9 +10,9 @@ def get_user(user_id):
     with open('users_db.json', 'r') as db_get:
         data_users = json.load(db_get)
     for user in data_users['users']:
-        if user_id == user['id']:
-            return str(user)
-    return None
+        if user_id == int(user['id']):
+            return str(user['city'])
+    return 'User does not exist.'
 
 
 def add_user(user_id, city):
@@ -21,42 +20,36 @@ def add_user(user_id, city):
         with open('users_db.json', 'r+') as db_add:
             data_users = json.load(db_add)
             for user in data_users['users']:
-                if user_id == user['id']:
+                if user_id == int(user['id']):
                     return False
-            data_users['users'].append({'id': int(user_id), 'city': city})
+            data_users['users'].append({'id': user_id, 'city': city})
             db_add.seek(0)
             json.dump(data_users, db_add)
             db_add.truncate()
             return True
     except NameError:
-        logging.exception('UserError')
+        return False
 
 
 def del_user(user_id, city):
     try:
         with open('users_db.json', 'r+') as user_remove:
             data_users = json.load(user_remove)
-            data_users['users'].remove({'id': user_id, 'city': str(city)})
             user_remove.seek(0)
+            data_users['users'].remove({'id': user_id, 'city': city})
             json.dump(data_users, user_remove)
             user_remove.truncate()
-        return True
+        return 'Deleted.'
     except ValueError:
-        logging.exception('UserError')
+        return 'Wrong city or your city not in list. Try again!'
 
 
 def update_user(user_id, city):
     try:
-        with open('users_db.json', 'r+') as user_update:
-            data_users = json.load(user_update)
-            for user in data_users['users']:
-                if user_id == user['id']:
-                    user_update.seek(0)
-                    data_users['users'].remove({'id': user_id, 'city': user['city']})
-                    data_users['users'].append({'id': int(user_id), 'city': city})
-                    user_update.seek(0)
-                    json.dump(data_users, user_update)
-                    user_update.truncate()
-                    return True
+        user_for_update = get_user(user_id)
+        del_user(user_id, user_for_update)
+        add_user(user_id, city)
+        return 'Updated'
     except ValueError:
-        logging.exception('UserError')
+        return 'User does not exist.'
+
