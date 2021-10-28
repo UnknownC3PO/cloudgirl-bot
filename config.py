6c21eed974d472fb603c5bf75924ba197c40c6eb
UnknownC3PO <bitsyuk123@gmail.com>
2021-10-28 18:14:07 +0300

    .gitignore is now working

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-#API_key = '6120a59d8e15952b6977b0cf445e2f9d'
-#TOKEN = '1965007379:AAGBRO-V47nUex5aY6wk6KA7Rc0YB33n-c8'
-#APP_NAME = 'cloudgirl-bot'
-API_key = os.environ('API_key')
-TOKEN = os.environ('TOKEN')
-APP_NAME = os.environ('APP_NAME')
